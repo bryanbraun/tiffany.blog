fd5045adff49326b167c174004bb716903cd884e
Tiffany White <hey@tiffanyrwhite.com>
2019-06-02 20:20:58 -0400

    Update something

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
     prefix: 'that-mac-nerd',
-    suffix: 'v8.6',
+    suffix: 'v8.7',
     precache: 'precache',
     runtime: 'runtime-cache'
   });
