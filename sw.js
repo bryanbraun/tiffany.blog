f591641a86cc80c5e2597d80b3b26afced747c7d
Tiffany White <hey@tiffanyrwhite.com>
2019-12-02 14:17:44 -0500

    Update something

--- sw.js
+++ sw.js
@@ -25,12 +25,12 @@ const OFFLINE_URL = 'offline.html';
 
 self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(ASSETS);
-    });
+    const cache = await caches.open(CACHE_NAME);
+
     // Setting {cache: 'reload'} in the new request will ensure that the response
     // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-    await cache.add(new Request(OFFLINE_URL, ASSETS { cache: 'reload' }));
+    await cache.add(new Request(OFFLINE_URL { cache: 'reload' }));
+    return cache.addAll(ASSETS);
   })());
 });
 
