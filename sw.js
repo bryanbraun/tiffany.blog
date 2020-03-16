a7bcf34cd76885db6579e3231a0e151faab79386
Tiffany White <hey@tiffanyrwhite.com>
2020-03-16 02:01:24 -0400

    Update something

--- sw.js
+++ sw.js
@@ -1,82 +1,43 @@
-/*
-Copyright 2015, 2019 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
+// set names for both precache & runtime cache
+workbox.core.setCacheNameDetails({
+    prefix: 'tiffany-white-blog',
+    suffix: 'v10.0',
+    precache: 'precache',
+    runtime: 'runtime-cache'
+  });
 
-// Incrementing OFFLINE_VERSION will kick off the install event and force
-// previously cached resources to be updated from the network.
-const OFFLINE_VERSION = 4;
-const CACHE_NAME = 'offline';
-const ASSETS = [
-  '/assets/css/offline.css',
-  '/assets/js/cats.js',
-  '/assets/images/tiffb.png'
-];
-// Customize this with a different URL if needed.
-const OFFLINE_URL = 'offline.html';
+  // let Service Worker take control of pages ASAP
+  workbox.skipWaiting();
+  workbox.clientsClaim();
 
-self.addEventListener('install', (event) => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    // Setting {cache: 'reload'} in the new request will ensure that the response
-    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-    await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
-    return cache.addAll(ASSETS);
-  })());
-});
+  // let Workbox handle our precache list
+  workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    // Enable navigation preload if it's supported.
-    // See https://developers.google.com/web/updates/2017/02/navigation-preload
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-  })());
+  // use `networkFirst` strategy for `*.html`, like all my posts
+  workbox.routing.registerRoute(
+    /\.html$/,
+    workbox.strategies.networkFirst()
+  );
 
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        // First, try to use the navigation preload response if it's supported.
-        const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
-        }
-
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      } catch (error) {
-        // catch is only triggered if an exception is thrown, which is likely
-        // due to a network error.
-        // If fetch() returns a valid HTTP response with a response code in
-        // the 4xx or 5xx range, the catch() will NOT be called.
-        console.log('Fetch failed; returning offline page instead.', error);
-
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        return cachedResponse;
+  workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
+    return articleHandler.handle(args).then(response => {
+      if (!response) {
+        return caches.match('pages/offline.html');
+      } else if (response.status === 404) {
+        return caches.match('pages/404.html');
       }
-    })());
-  }
+      return response;
+    });
+  });
 
-  // If our if() condition is false, then this fetch handler won't intercept the
-  // request. If there are any other fetch handlers registered, they will get a
-  // chance to call event.respondWith(). If no fetch handlers call
-  // event.respondWith(), the request will be handled by the browser as if there
-  // were no service worker involvement.
-});
+  // use `cacheFirst` strategy for images
+  workbox.routing.registerRoute(
+    /assets\/(img|icons)/,
+    workbox.strategies.cacheFirst()
+  );
+
+  // third party files
+  workbox.routing.registerRoute(
+    /^https?:\/\/cdn.staticfile.org/,
+    workbox.strategies.staleWhileRevalidate()
+  )
